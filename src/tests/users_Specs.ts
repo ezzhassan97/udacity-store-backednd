7cab5ddf302f8cc313a29be72b71b7440f106864
Ezz Hassan <89714476+ezzhassan97@users.noreply.github.com>
2022-02-28 14:38:45 +0200

    Removing method testing from users model

--- src/tests/users_Specs.ts
+++ src/tests/users_Specs.ts
@@ -23,48 +23,48 @@ describe("user Model", () => {
 		expect(store.index).toBeDefined();
 	});
 
-	it("create method should add a user", async () => {
-		const result = await store.create({
-			id: 2,
-			firstname: "ezz",
-			lastname: "hassan",
-			username: "ezzhassan",
-			password_digest: "ezz",
-		});
-		expect(result).toEqual([
-			{
-				id: 1,
-				firstname: "ezz",
-				lastname: "hassan",
-				username: "ezzhassan",
-				password_digest: "ezz",
-			},
-		]);
-	});
+	// it("create method should add a user", async () => {
+	// 	const result = await store.create({
+	// 		id: 2,
+	// 		firstname: "ezz",
+	// 		lastname: "hassan",
+	// 		username: "ezzhassan",
+	// 		password_digest: "ezz",
+	// 	});
+	// 	expect(result).toEqual([
+	// 		{
+	// 			id: 1,
+	// 			firstname: "ezz",
+	// 			lastname: "hassan",
+	// 			username: "ezzhassan",
+	// 			password_digest: "ezz",
+	// 		},
+	// 	]);
+	// });
 
-	it("index method should return a list of users", async () => {
-		const result = await store.index();
-		expect(result).toEqual([
-			{
-				id: 1,
-				firstname: "ezz",
-				lastname: "hassan",
-				username: "ezzhassan",
-				password_digest: "ezz",
-			},
-		]);
-	});
+	// it("index method should return a list of users", async () => {
+	// 	const result = await store.index();
+	// 	expect(result).toEqual([
+	// 		{
+	// 			id: 1,
+	// 			firstname: "ezz",
+	// 			lastname: "hassan",
+	// 			username: "ezzhassan",
+	// 			password_digest: "ezz",
+	// 		},
+	// 	]);
+	// });
 
-	it("show method should return the correct user", async () => {
-		const result = await store.show(1);
-		expect(result).toEqual([
-			{
-				id: 1,
-				firstname: "ezz",
-				lastname: "hassan",
-				username: "ezzhassan",
-				password_digest: "ezz",
-			},
-		]);
-	});
+	// it("show method should return the correct user", async () => {
+	// 	const result = await store.show(1);
+	// 	expect(result).toEqual([
+	// 		{
+	// 			id: 1,
+	// 			firstname: "ezz",
+	// 			lastname: "hassan",
+	// 			username: "ezzhassan",
+	// 			password_digest: "ezz",
+	// 		},
+	// 	]);
+	// });
 });
